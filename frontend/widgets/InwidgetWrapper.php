3ce471091562d3a8bb0b57f47a8f4aac9932b1f2
Geksor <geksor@gmail.com>
2019-01-24 12:25:32 +0300

    work geksor

--- frontend/widgets/InwidgetWrapper.php
+++ frontend/widgets/InwidgetWrapper.php
@@ -2,10 +2,13 @@
 
 namespace frontend\widgets;
 
+use yii\helpers\VarDumper;
+
 class InwidgetWrapper extends \yii\base\Widget
 {
     public function run()
     {
+        VarDumper::dump($_SERVER['DOCUMENT_ROOT'], 10, true);
         return include $_SERVER['DOCUMENT_ROOT'].'/frontend/web/inwidget/template.php';
     }
 }
