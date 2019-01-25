93c83f7f391d698eb6834231b2443cef8ea5a831
Geksor <geksor@gmail.com>
2019-01-25 09:10:30 +0300

    work geksor

--- frontend/views/layouts/footer.php
+++ frontend/views/layouts/footer.php
@@ -22,12 +22,4 @@
             <a href="tel:<?= array_key_exists('phone', Yii::$app->params['Contact'])?Yii::$app->params['Contact']['phone']:'' ?>" style="text-decoration: none"><?= array_key_exists('phone', Yii::$app->params['Contact'])?Yii::$app->params['Contact']['phone']:'' ?></a>
         </div>
     </div>
-    <div class="rosReestrWidget" style="padding: 20px 0; display: flex; justify-content: center">
-        <script type="text/javascript" src="https://russiatourism.ru/operators/widget/js/widget.js"></script>
-        <!-- Russiatourism.ru Widget -->
-        <div id="russiatourism_widget"></div>
-        <script type="text/javascript">
-            RT.Widget.build('%D0%A0%D0%A2%D0%9E+020279');
-        </script>
-    </div>
 </footer><!-- .footer -->
